cb5c91ee24eeb52000d35cae1f35e149f6fed053
AJ <alexanderjessediaz@gmail.com>
2020-11-25 14:49:46 -0700

    in index.js re define io with cors

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const app = require('express')() // <---  currying
-const http = require("http").createServer(app)
-const io = require("socket.io")(http)
+const app = require('express')(); // <---  currying
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+    cors: {
+        origin: "*",
+        methods: "*"
+    }
+})
+
 
 io.on("connection", socket => {
     console.log("A user connected")
-})
-
-app.get("/", (request, response) => {
-    response.send("<h1>Hello World</h1>")
-})
+});
 
 http.listen(4000, () => {
     console.log("Listening on port 4000")
-})
+});
