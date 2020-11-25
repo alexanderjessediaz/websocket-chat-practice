790689c9a9ad7f7e36e98c7b2d94b85deb254227
AJ <alexanderjessediaz@gmail.com>
2020-11-25 14:30:34 -0700

    add lite-server and modify packge.json for dev run

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const app = require('express')() // <---  currying
 const http = require("http").createServer(app)
+const io = require("socket.io")(http)
+
+io.on("connection", socket => {
+    console.log("A user connected")
+})
 
 app.get("/", (request, response) => {
     response.send("<h1>Hello World</h1>")
